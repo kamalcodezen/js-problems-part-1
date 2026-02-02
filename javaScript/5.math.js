66717f82fc36dfa8d539403d0b83655948e887a8
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-02 16:50:45 +0530

    math practice

---
+++ javaScript/5.math.js
@@ -0,0 +1,32 @@
+
+const nums = [2, 5, 87, 59, 79, 92];
+
+const maxArr = Math.max(...nums); //Array biggest value
+console.log(maxArr);
+
+const minArr = Math.min(...nums); // Array smallest value
+console.log(minArr)
+
+
+const smallNumber = Math.min(23, 56, 76, 454, 46, 23, 53); //smallest number find
+console.log(smallNumber);
+
+const bigNumber = Math.max(67, 46, 35, 9, 35, 24, 24,); //biggest number find
+console.group(bigNumber);
+
+console.log(Math.abs(5 - 9)); // number er distance access
+
+console.log(Math.round(6.05));//nearest integer number find 
+console.log(Math.round(6.90));//nearest integer number find 
+
+console.log(Math.floor(3.99))// integer number return
+
+console.log(Math.ceil(4.78)) // up integer number return
+
+console.log(Math.PI)
+
+console.log(Math.random() * 10);
+
+const randomNums = Math.round(Math.random() * 10); // random number find lottery and  game work intger dokar hole function er bhetore relkhe debo ;
+console.log(randomNums); 
+
